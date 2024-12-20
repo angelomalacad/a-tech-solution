a694424d8e389797db5437d269c424b8735be54b
Arabella <jordanfaearabella@gmail.com>
2024-12-21 04:12:26 +0800

    our team

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
-// const burgerIcon = document.getElementsByClassName("mobile-nav")[0];
-// const mobileMenu = document.getElementsByClassName("div-nav")[0];
-
-// burgerIcon.addEventListener("click", function(){
-//     mobileMenu.classList.toggle("show");
-// });
-
 document.querySelector('.hamburger').addEventListener('click', function() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active'); 
   });
 
-
-  // carousel
-
-  carouselImage = document.getElementsByClassName("repair-img")[0];
-  
