072f1fb0e68119d41d5804b87804184b563d1338
Arabella <jordanfaearabella@gmail.com>
2024-12-21 00:34:42 +0800

    home section

--- js/services.js
+++ js/services.js
@@ -1,6 +1,11 @@
-const burgerIcon = document.getElementsByClassName("mobile-nav")[0];
-const mobileMenu = document.getElementsByClassName("div-nav")[0];
+// const burgerIcon = document.getElementsByClassName("mobile-nav")[0];
+// const mobileMenu = document.getElementsByClassName("div-nav")[0];
 
-burgerIcon.addEventListener("click", function(){
-    mobileMenu.classList.toggle("show");
-});
+// burgerIcon.addEventListener("click", function(){
+//     mobileMenu.classList.toggle("show");
+// });
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); 
+  });
